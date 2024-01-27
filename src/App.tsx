9d63f23eaea6e62fc107c5754948d1d8d9637575
Magomed <tatarkhanov.magomed@inbox.ru>
2024-01-27 16:14:01 +0300

    cart

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React, { FC } from 'react'
+import { Routes, Route } from 'react-router-dom'
 
 import './App.css'
 import Home from './pages/Home'
+import Cart from './pages/Cart/Cart'
 
 const App: FC = () => {
   return (
     <div className="App">
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
     </div>
   )
 }
